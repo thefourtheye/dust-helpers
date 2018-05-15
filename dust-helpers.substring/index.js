8eea94db55fb53bb305c3fd6b2a117f97f61e539
Sakthipriyan Vairamani (thefourtheye) <thechargingvolcano@gmail.com>
2018-05-15 15:15:47 +0530

    enable prettier

--- dust-helpers.substring/index.js
+++ dust-helpers.substring/index.js
@@ -1,25 +1,23 @@
-(function (dust) {
+(function(dust) {
   'use strict';
 
-  dust.helpers.substring = function (chunk, ctx, bodies, params) {
+  dust.helpers.substring = function(chunk, ctx, bodies, params) {
     var string = dust.helpers.tap(params.string, chunk, ctx);
     var start = dust.helpers.tap(params.start, chunk, ctx);
     var end = dust.helpers.tap(params.end, chunk, ctx);
 
     if (!string || typeof string !== 'string') {
-        return chunk.write('');
+      return chunk.write('');
     }
 
     if (typeof start !== 'number' || start < 0) {
-        start = 0;
+      start = 0;
     }
 
     if (typeof end !== 'number' || end > string.length) {
-        end = string.length;
+      end = string.length;
     }
 
     return chunk.write(string.substring(start, end));
   };
-
-})(typeof exports !== 'undefined' ? module.exports = require('dustjs-linkedin') : dust);
-
+})(typeof exports !== 'undefined' ? (module.exports = require('dustjs-linkedin')) : dust);
